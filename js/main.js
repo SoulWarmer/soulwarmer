eecefa47f21001cbdd0db30c49210242fd51d542
SoulWarmer <soulwarmerpodcast@gmail.com>
2026-04-27 08:07:05 +0700

    Fix fade-up observer to include HTML elements

--- js/main.js
+++ js/main.js
@@ -53,3 +53,7 @@ document.querySelectorAll(FADE_SELECTORS).forEach(el => {
   el.classList.add('fade-up');
   observer.observe(el);
 });
+
+document.querySelectorAll('.fade-up').forEach(el => {
+  observer.observe(el);
+});
